dc74f60d9a31a8e5569986bb2588b64c85543bcb
George Arnold <george.arnold.88@gmail.com>
2021-07-13 12:55:19 +0000

    link?

--- map.js
+++ map.js
@@ -1,8 +1,6 @@
 		$('.map').maphilight();
-		console.log('connected');
 		$(document).ready(function () {
 			$(document).on('mousemove', function (e) {
-				console.log('1');
 				$('.title').css({
 					left: e.pageX,
 					top: e.pageY,
@@ -10,7 +8,6 @@
 			});
 			$('.map-name').hover(
 				function () {
-					console.log('functional');
 					$('.title').show();
 					$('.title').text($(this).attr('title'));
 				},
